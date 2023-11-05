f9d6835be4dceda62888a6f2a0c8b37fc0a493bc
if22b086@technikum-wien.at <lilligruber02@gmail.com>
2023-11-05 12:09:41 +0100

    tests added

---
+++ MTCG.test/TestModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MTCG;
+using MTCG.Models;
+
+namespace MTCG.test
+{
+    [TestFixture]
+    internal class TestModels
+    {
+        [SetUp]
+        public void Setup()
+        {
+            //Wird vor jedem test ausgeführt
+
+        }
+
+        [Test]
+        public void TestRandomCards()
+        {
+            Package testPackage = new Package();
+
+            Card randCard = testPackage.GenerateCard();
+
+            Console.WriteLine(randCard.ToString());
+
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            //Wird nach jedem Test ausgeführt
+        }
+    }
+}
